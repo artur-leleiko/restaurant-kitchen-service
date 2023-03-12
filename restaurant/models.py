14b258c7a296440bb0932bfd738eeff6386f1d30
Artur Leleiko <artur_98@ukr.net>
2023-03-12 16:23:28 +0200

    Registered all models in the admin + migrated

--- restaurant/models.py
+++ restaurant/models.py
@@ -14,7 +14,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True)
 
     class Meta:
         ordering = ["username"]
