73b0d3630527c30f2f47760cc476f26eaa5b951f
Artur Leleiko <artur_98@ukr.net>
2023-03-13 16:10:13 +0200

    Added index in urls.py and views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from restaurant.models import DishType, Cook, Dish
+
+
+def index(request):
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes
+    }
+
+    return render(request, "restaurant/index.html", context=context)
