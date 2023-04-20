909c242b495996487f6aca9236d880aaebb4944d
Artur Leleiko <artur_98@ukr.net>
2023-04-20 21:29:11 +0300

    Updated Debug in settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-esx6.onrender.com"]
 
